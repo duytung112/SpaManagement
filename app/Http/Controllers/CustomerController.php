3f176383d237e5bc196fb693b81753fe51343b25
duytung112 <duytung98@gmail.com>
2020-08-29 14:26:42 +0700

    create view and controller for customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,7 +14,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        return view('customer.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return view('customer.create');
     }
 
     /**
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Index customer</h1>
+    </div>
+@endsection
