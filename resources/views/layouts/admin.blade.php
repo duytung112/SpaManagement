61f01d7eb97b12eda097346cf2e108d571d857d9
duytung112 <duytung98@gmail.com>
2020-09-06 22:37:58 +0700

    add yeild blade to title and main content

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -292,12 +292,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Dashboard</h1>
+                        <h1 class="m-0 text-dark">@yield('title')</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item active">@yield('title')</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -308,6 +308,7 @@
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
+                @yield('content')
             </div><!-- /.container-fluid -->
         </section>
         <!-- /.content -->
